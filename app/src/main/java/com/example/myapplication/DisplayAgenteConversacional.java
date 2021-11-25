91121b086dc4ece671242c699d8578fa2f72e7b7
albertorobles2000 <alberto.robles.hernandez@gmail.com>
2021-11-25 09:03:13 +0100

    Voz

--- app/src/main/java/com/example/myapplication/DisplayAgenteConversacional.java
+++ app/src/main/java/com/example/myapplication/DisplayAgenteConversacional.java
@@ -1,5 +1,10 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,47 +13,47 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class DisplayAgenteConversacional extends AppCompatActivity {
-
-    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
-
+    public static final Integer RecordAudioRequestCode = 1;
     private SpeechRecognizer speechRecognizer;
+    private TextToSpeech textToSpeechEngine;
     private EditText editText;
     private ImageView micButton;
+    private Button ttsButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_display_agente_conversacional);
-
-        //Check de permiso
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                //vemos que la versión de android es al menos M para mostrar el dialogo de solicitud de audio
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO);
-            }
-            return;
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            checkPermission();
         }
+
         editText = findViewById(R.id.speechtext);
         micButton = findViewById(R.id.speechbutton);
+        //ttsButton = findViewById(R.id.tts);
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        textToSpeechEngine = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.SUCCESS) {
+                    Log.e("TTS", "Inicio de la síntesis fallido");
+                }
+            }
+        });
+
 
         final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -56,24 +61,23 @@ public class DisplayAgenteConversacional extends AppCompatActivity {
 
         speechRecognizer.setRecognitionListener(new RecognitionListener() {
             @Override
-            public void onReadyForSpeech(Bundle params) {
+            public void onReadyForSpeech(Bundle bundle) {
 
             }
 
             @Override
             public void onBeginningOfSpeech() {
                 editText.setText("");
-                editText.setHint("Escuchando...");
+                editText.setHint("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
 
             }
 
             @Override
-            public void onRmsChanged(float rmsdB) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] buffer) {
+            public void onBufferReceived(byte[] bytes) {
 
             }
 
@@ -83,39 +87,49 @@ public class DisplayAgenteConversacional extends AppCompatActivity {
             }
 
             @Override
-            public void onError(int error) {
+            public void onError(int i) {
 
             }
 
             @Override
             public void onResults(Bundle bundle) {
-                micButton.setImageResource(R.drawable.ic_mic_black_off3);
+                micButton.setImageResource(R.drawable.ic_mic_black_off);
                 ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 editText.setText(data.get(0));
             }
 
             @Override
-            public void onPartialResults(Bundle partialResults) {
+            public void onPartialResults(Bundle bundle) {
 
             }
 
             @Override
-            public void onEvent(int eventType, Bundle params) {
+            public void onEvent(int i, Bundle bundle) {
 
             }
         });
+
         micButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction()== MotionEvent.ACTION_UP){
-                    micButton.setImageResource(R.drawable.ic_mic_black_off3);
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
                     speechRecognizer.stopListening();
                 }
-                if (event.getAction() == MotionEvent.ACTION_DOWN){
-                    micButton.setImageResource(R.drawable.ic_mic_black_off3_pressed);
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    micButton.setImageResource(R.drawable.ic_mic_black_off3);
                     speechRecognizer.startListening(speechRecognizerIntent);
                 }
-                return true;
+                return false;
+            }
+        });
+
+        ttsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String text = String.valueOf(editText.getText());
+                if (!text.isEmpty())
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        textToSpeechEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts1");
+                    }
             }
         });
     }
@@ -126,14 +140,18 @@ public class DisplayAgenteConversacional extends AppCompatActivity {
         speechRecognizer.destroy();
     }
 
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions,
-                grantResults);
-        if (requestCode == MY_PERMISSIONS_RECORD_AUDIO && grantResults.length > 0 ){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
         }
     }
-
 }
